49ffbde13e6097cd74f83a2fd2edd52d80582e46
Nusayb <mansury2@illinois.edu>
2022-05-08 09:57:16 -0500

    end episode 12 (7) made 2 sprites

--- BeardedGameEngine_core/main.cpp
+++ BeardedGameEngine_core/main.cpp
@@ -3,7 +3,11 @@
 #include "x64\src\utils\fileutils.h"
 #include "x64\src\graphics\shader.h"
 
-int main()	
+#include "x64\src\graphics\buffers\buffer.h"
+#include "x64\src\graphics\buffers\indexbuffer.h"
+#include "x64\src\graphics\buffers\vertexarray.h"
+
+int main()
 {
 	using namespace beardedGameEngine;
 	using namespace graphics;
@@ -11,7 +15,8 @@ int main()
 
 	Window window("BeardedGameEngine", 800, 600);
 	//glClearColor(0.5f, 0.1f, 0.7f, 1.0f);
-	
+
+	#if 0
 	GLfloat vertices[] =
 	{
 		4, 3, 0,
@@ -21,21 +26,56 @@ int main()
 		12, 6, 0,
 		4, 3, 0
 	};
-
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
+	#else
+	GLfloat vertices[] =
+	{
+		4, 3, 0,
+		12, 3, 0,
+		4, 6, 0,
+		12, 6, 0,
+	};
+	GLushort indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
+	};
+	GLfloat colorsA[] =
+	{
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+		1, 0, 1, 1,
+	};
+	GLfloat colorsB[] =
+	{
+		0, 0, 1, 1,
+		0, 0, 1, 1,
+		0, 0, 1, 1,
+		0, 0, 1, 1,
+	};
+	VertexArray sprite1, sprite2;
+	IndexBuffer ibo(indices, 6);
+
+	sprite1.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+	sprite1.addBuffer(new Buffer(colorsA, 4 * 4, 4), 1);
+	sprite2.addBuffer(new Buffer(vertices, 4 * 3, 3), 0);
+	sprite2.addBuffer(new Buffer(colorsB, 4 * 4, 4), 1);
+	#endif
 
 	mat4 ortho = mat4::orthograhic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 
 	Shader shader("x64/src/shaders/basic.vert", "x64/src/shaders/basic.frag");
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
+	//shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
 
-	shader.setUniform2f("light_pos", vec2(8.0f, 4.5f));
+	shader.setUniform2f("light_pos", vec2(15.0f, 4.5f));
 	shader.setUniform4f("colour", vec4(1.0f, 0.0f, 0.0f, 0.1f));
 
 	/*
@@ -54,9 +94,30 @@ int main()
 	while (!window.closed()) {
 		window.clear();
 		
+		double x, y;
+		window.getMousePosition(x, y);
+		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 960.0f), (float)(9.0f - y * 9.0f / 540.0f)));
+
+		#if 0
 		glDrawArrays(GL_TRIANGLES, 0, 6);
+		#else
+		sprite1.bind();
+		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(4, 3, 0)));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+		ibo.bind();
+		sprite1.unbind();
+
+		sprite2.bind();
+		ibo.bind();
+		shader.setUniformMat4("ml_matrix", mat4::translation(vec3(0, 0, 0)));
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT, 0);
+		ibo.bind();
+		sprite2.unbind();
+		#endif
 
 		window.update();
+
 	}
 
 	return 0;
