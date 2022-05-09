8f1ac13a3ca143ffaed808c102a07e374499cb7f
Nusayb <mansury2@illinois.edu>
2022-05-09 16:33:12 -0500

    batchrenderer2d works boio

--- BeardedGameEngine_core/main.cpp
+++ BeardedGameEngine_core/main.cpp
@@ -13,6 +13,11 @@
 #include "x64\src\graphics\static_sprite.h"
 #include "x64\src\graphics\sprite.h"
 
+#include <vector>
+#include <time.h>
+
+#define BATCH_RENDERER 1
+
 int main()
 {
 	using namespace beardedGameEngine;
@@ -26,9 +31,34 @@ int main()
 	shader.enable();
 	shader.setUniformMat4("pr_matrix", ortho);
 
-	Sprite sprite(5, 5, 4, 4, maths::vec4(1, 0, 1, 1));
-	Sprite sprite2(7, 1, 2, 3, maths::vec4(0, 1, 0, 1));
+	std::vector<Renderable2D*> sprites;
+	srand(time(NULL));
+
+	for (float y = 0; y < 9.0f; y+=0.1) {
+		for (float x = 0; x < 16.0f; x+=0.1) {
+			sprites.push_back(new 
+			#if BATCH_RENDERER
+			Sprite
+			#else
+			StaticSprite
+			#endif
+			(x, y, 0.09f, 0.09f, maths::vec4(rand() % 1000 / 1000.0f, 0, rand() % 1000 / 1000.0f, 1)
+			#if !BATCH_RENDERER
+			, shader
+			#endif
+			));
+		}
+	}
+
+	#if BATCH_RENDERER
+	//Sprite sprite(5, 5, 4, 4, maths::vec4(1, 0, 1, 1));
+	//Sprite sprite2(7, 1, 2, 3, maths::vec4(0, 1, 0, 1));
 	BatchRenderer2D renderer;
+	#else
+	//StaticSprite sprite(5, 5, 4, 4, maths::vec4(1, 0, 1, 1), shader);
+	//StaticSprite sprite2(7, 1, 2, 3, maths::vec4(0, 1, 0, 1), shader);
+	Simple2DRenderer renderer;
+	#endif
 
 	while (!window.closed()) {
 		window.clear();
@@ -37,10 +67,15 @@ int main()
 		window.getMousePosition(x, y);
 		shader.setUniform2f("light_pos", vec2((float)(x * 16.0f / 1280.0f), (float)(9.0f - y * 9.0f / 720.0f)));
 
+		#if BATCH_RENDERER
 		renderer.begin();
-		renderer.submit(&sprite);
-		renderer.submit(&sprite2);
+		#endif
+		for (int i = 0; i < sprites.size(); i++) {
+			renderer.submit(sprites[i]);
+		}
+		#if BATCH_RENDERER
 		renderer.end();
+		#endif
 		renderer.flush();
 
 		window.update();
