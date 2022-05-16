16ae99bda66dd232147d60ae3bc1d8527fe2f9a8
Nusayb <mansury2@illinois.edu>
2022-05-15 20:11:38 -0500

    textures working boio

--- BeardedGameEngine_core/main.cpp
+++ BeardedGameEngine_core/main.cpp
@@ -35,37 +35,20 @@ int main()
 	Shader* shader = new Shader("x64/src/shaders/basic.vert", "x64/src/shaders/basic.frag");
 	Shader& s = *shader;
 
-	Shader* shader2 = new Shader("x64/src/shaders/basic.vert", "x64/src/shaders/basic.frag");
-	Shader& s2 = *shader2;
-
 	TileLayer layer(shader);
-	for (float y = -8.9f; y < 9.0f; y += 1) {
-		for (float x = -15.9f; x < 16.0f; x += 1) {
-			layer.add(new Sprite(x, y, 0.8f, 0.8f, maths::vec4(rand() % 1000 / 1000.0f, 0, rand() % 1000 / 1000.0f, 1)));
+	for (float y = -8.9f; y < 9.0f; y += 3) {
+		for (float x = -15.9f; x < 16.0f; x += 3) {
+			layer.add(new Sprite(x, y, 2.8f, 2.8f, maths::vec4(rand() % 1000 / 1000.0f, 0, rand() % 1000 / 1000.0f, 1)));
 		}
 	}
 
 	Timer time;
 	float timer = 0.0f;
 	unsigned int frames = 0;
-	
-	mat4 transform = mat4::translation(vec3(-15.0f, 5.0f, 0.0f)) * mat4::rotation(50.0f, vec3(0, 0, 1));
 
-	Group* group = new Group(transform);
-	group->add(new Sprite(0, 0, 6, 3, maths::vec4(1, 1, 1, 1)));
-
-	Group* button = new Group(mat4::translation(vec3(0.5f, 0.5f, 0.0f)));
-	button->add(new Sprite(0, 0, 5.0f, 2.0f, maths::vec4(1, 0, 1, 1)));
-	button->add(new Sprite(0.5f, 0.5f, 3, 1, maths::vec4(0.2f, 0.3f, 0.8f, 1)));
-	group->add(button);
-
-	layer.add(group);
-
-	Texture texture("test.png");
+	glActiveTexture(GL_TEXTURE0);
+	Texture texture("alto.png");
 	texture.bind();
-
-	TileLayer layer2(shader2);
-	layer2.add(new Sprite(-2, -2, 4.0f, 4.0f, maths::vec4(0, rand() % 1000 / 1000.0f, rand() % 1000 / 1000.0f, 1)));
 
 	while (!window.closed()) {
 
@@ -76,11 +59,8 @@ int main()
 
 		s.enable();
 		s.setUniform2f("light_pos", vec2((float)(x * 32.0f / 1280.0f - 16.0f), (float)(9.0f - y * 18.0f / 720.0f)));
-		s2.enable();
-		s2.setUniform2f("light_pos", vec2((float)(x * 32.0f / 1280.0f - 16.0f), (float)(9.0f - y * 18.0f / 720.0f)));
 
 		layer.render();
-		layer2.render();
 
 		window.update();
 
--- BeardedGameEngine_core/x64/src/graphics/batchrenderer2d.h
+++ BeardedGameEngine_core/x64/src/graphics/batchrenderer2d.h
@@ -13,7 +13,8 @@ namespace beardedGameEngine { namespace graphics {
 	#define RENDERER_INDICES_SIZE RENDERER_MAX_SPRITES * 6
 
 	#define SHADER_VERTEX_INDEX 0
-	#define SHADER_COLOR_INDEX  1
+	#define SHADER_UV_INDEX  1 //corresponds to shader "2" for texture
+	#define SHADER_COLOR_INDEX  2
 
 	class BatchRenderer2D : public Renderer2D
 	{
